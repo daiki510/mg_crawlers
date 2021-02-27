596c055c5befc61f90873d87f11a6433dcf6ebd0
daiki510 <ddgg7755@gmail.com>
2021-02-27 22:40:44 +0900

    puppeteerの導入

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
-function hello(name: string): string {
-  return `Hello, ${name}!`;
+const puppeteer  = require('puppeteer');
+
+const main = async () => {
+  console.log('poyo');
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  
+  
+  const url = 'https://google.com/'
+  await page.goto(url, { waitUntil: 'networkidle0' });
+
+  // await page.screenshot({ path: 'home1.png' });
+
+  // await page.title() でも良い
+  const title = await page.title();
+  console.log("============================")
+  console.log(title)
+
+  await browser.close();
 }
 
-console.log(hello("World"));
+main();
