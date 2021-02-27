3ccf47ea6adf97a04dc7d2278588c61b517c6c39
daiki510 <ddgg7755@gmail.com>
2021-02-27 23:26:51 +0900

    puppeteerの拡張

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,33 @@
 const puppeteer  = require('puppeteer');
+const headless = false;
+const screenSize = {width: 1920, height: 1080};
+const slowMo = 0;
+const args = [
+  // '--start-fullscreen',
+  '--disable-infobars',
+  '--incognito',
+];
 
 const main = async () => {
-  console.log('poyo');
-  const browser = await puppeteer.launch();
+  console.log('start');
+  const browser = await puppeteer.launch({headless, slowMo, args});
   const page = await browser.newPage();
+  await page.setViewport(screenSize);
   
-  
-  const url = 'https://google.com/'
+  const url = 'https://qiita.com/'
   await page.goto(url, { waitUntil: 'networkidle0' });
-
-  // await page.screenshot({ path: 'home1.png' });
+  // await page.type('input[title="検索"]', "puppeteer", { delay: 100 });
+  // await page.click('input[value^="Google"]');
+  // await page.waitForNavigation({timeout: 600000, waitUntil: "domcontentloaded"});
+  await page.screenshot({ path: 'storage/screenshot.png' });
 
   // await page.title() でも良い
   const title = await page.title();
-  console.log("============================")
-  console.log(title)
-
+  console.log("============================");
+  console.log(title);
+  
   await browser.close();
+  console.log('end');
 }
 
 main();
