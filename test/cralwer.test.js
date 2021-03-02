945e2a34a248731da1439bb8c7adf1a5dca049ca
daiki510 <ddgg7755@gmail.com>
2021-03-02 22:13:40 +0900

    テストの追加

--- test/cralwer.test.js
+++ test/cralwer.test.js
@@ -1,16 +1,25 @@
-// const crawling = require('../src/crawler');
-// const mangaList = require('../src/mangaList');
 const topPageUrl = 'https://manga1000.com/';
+const selectors = {
+  searchForm: '#sticky-wrapper > div > div > div.search-holder > div > form > input',
+  searchButton: '#sticky-wrapper > div > div > div.search-holder > div > form > button',
+  chapterLink: '#post-22224 > div > div.featured-thumb > a',
+}
+
+const xPath = {
+  latestChapter: `//div[@class='chaplist']/table[@class='table table-hover']/tbody/tr[1]/td/p/a`
+}
 
 describe('Manga Raw', () => {
-  // const resutls = crawling();
-  // console.log(resutls);
   beforeAll(async () => {
-    // console.log(mangaList);
     await page.goto(topPageUrl);
   });
 
-  it('should be titled "Manga Raw"', async () => {
+  it('タイトルが「Manga Raw」であること', async () => {
     await expect(page.title()).resolves.toContain('Manga Raw');
+  });
+
+  it('検索フォームに入力し、検索ができること', async () => {
+    await expect(page).toFill(selectors.searchForm, '呪術廻戦')
+    await expect(page).toClick(selectors.searchButton)
   });
 });
